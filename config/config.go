54f3a5bb2b39945b3f916cfe27c9c95c8dd1f088
Eduardy Lopes <eduardylopes@gmail.com>
2023-07-20 19:55:58 -0300

    feat: add database handler

--- config/config.go
+++ config/config.go
@@ -1,14 +1,32 @@
 package config
 
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
 var (
-	l *Logger
+	l  *Logger
+	db *gorm.DB
 )
 
 func Init() error {
+	var err error
+	db, err = InitializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("error initializing sqlite: %v", err)
+	}
+
 	return nil
 }
 
 func GetLogger(p string) *Logger {
 	l = NewLogger(p)
 	return l
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
