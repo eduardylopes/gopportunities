a4ba00df09d93b7a2db620646850da5600fe1e0f
Eduardy Lopes <eduardylopes@gmail.com>
2023-07-21 20:00:56 -0300

    feat: add show handler

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,1 +1,38 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/eduardylopes/gopportunities/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+func ShowOpeningHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusNotFound, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	OpeningResponse := schemas.OpeningResponse{
+		ID:       opening.ID,
+		Role:     opening.Role,
+		Location: opening.Location,
+		Company:  opening.Company,
+		Remote:   opening.Remote,
+		Link:     opening.Link,
+		Salary:   opening.Salary,
+		CreateAt: opening.CreatedAt,
+		UpdateAt: opening.UpdatedAt,
+	}
+
+	sendSuccess(ctx, "show-opening", OpeningResponse)
+}
